767875ac3eed18c5c5ce2eda59741e26f865bb62
RISKI <Riski180722@gmail.com>
2022-08-30 16:01:17 +0700

    Mengumpulkan tugas C++ materi oprasi perkalian

---
+++ Tugas C++ materi oprasi perkalian.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A = 20;
+    int B = 10;
+    int multiplication;
+
+    multiplication = A*B;
+
+    cout<< multiplication;
+
+    return 0;
+}
